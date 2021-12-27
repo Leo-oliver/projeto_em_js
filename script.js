b98c4ce26188c4d53d4cf1ad2ff2ea08f031976e
Leandro de Oliveira <leandro.teodoro.oliveira@hotmail.com>
2021-12-27 13:20:11 -0300

    Adicionando o JavaScript

---
+++ script.js
@@ -0,0 +1,61 @@
+//função que adiciona tarefa
+function addTask(){
+
+    //Título da tarefa
+    const taskTitle = document.querySelector("#task-title").value 
+
+    if(taskTitle){
+        //clonar o template
+        const template = document.querySelector(".template")
+
+        const newTask = template.cloneNode(true)
+
+        //adiciona título
+        newTask.querySelector(".task-title").textContent = taskTitle
+
+        //remover as classes desnecessárias
+        newTask.classList.remove("template")
+        newTask.classList.remove("hide")
+
+        //adicionar tarefa na lista
+        const list = document.querySelector("#task-list")
+
+        list.appendChild(newTask)
+
+        //adicionar o evento de remover
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function(){
+            removeTask(this)
+        })
+
+        //adicionar evento de completar tarefa
+        const doneBtn = newTask.querySelector(".done-btn").addEventListener("click", function(){
+            completeTask(this)
+        })
+
+        //limpar texto
+        document.querySelector("#task-title").value = ""
+
+    }
+}
+
+//função de remover tarefa
+function removeTask(task){
+    task.parentNode.remove(true)
+}
+
+//função de completar a tarefa
+function completeTask(task){
+    const taskToComplete = task.parentNode
+    
+    taskToComplete.classList.toggle("done")  //toggle se tiver com a classe (done) ele remove no elemento se estiver sem ele poe a classe no elemento, faz o papel de if/else
+
+}
+//evento de adicionar tarefa
+const addBtn = document.querySelector("#add-btn")
+
+addBtn.addEventListener("click", function(e){
+
+    e.preventDefault()
+
+    addTask()
+})
